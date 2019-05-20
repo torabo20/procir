616a81f7b9e3cb407da5c0b4bc94c69b22c92f5b
torabo20 <tetsuo.kasuya@gmail.com>
2019-05-20 12:10:22 +0900

    add new files

---
+++ display.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<body>
+<p>受信データ</p>
+<?php echo $_GET['comment'] ?>
+</body>
+</html>
+
---
+++ form.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+<p>フォームからデータの送信</p>
+<form action="display.php" method="get">
+<input type="text" name="comment">
+<input type="submit" value="送信">
+</form>
+</body>
+</html>
+
